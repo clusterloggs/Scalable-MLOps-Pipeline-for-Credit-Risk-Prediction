9370845b8fc200f18b67acda895a750c06e30bb6
Clusterloggs <azkintonde@gmail.com>
2025-09-03 15:58:02 +0100

    pushing through from dev after 2 months

--- src/evaluation.py
+++ src/evaluation.py
@@ -6,6 +6,8 @@ import logging
 import mlflow
 import mlflow.sklearn
 
+
+
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
 
